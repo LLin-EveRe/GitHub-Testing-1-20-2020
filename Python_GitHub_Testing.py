7ed29ab0fec074fbd920d677afe9fa23ae71d46e
Sarah Post <spost1207@gmail.com>
2020-01-20 13:41:45 -0500

    Create Python_GitHub_Testing.py

---
+++ Python_GitHub_Testing.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 20 13:17:33 2020
+
+@author: spost
+"""
+
+
+#Print Hello
+print("hello")
+
+input1 = input()
+
+print("How's the weather outside today?")
+
+input2 = str(input())
+
+if input2 == "rainy":
+    print("you better grab an umbrella!")
+else:
+    print("yay! that's awesome")
+    
